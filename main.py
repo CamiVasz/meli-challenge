20afa7c086fb74c9c806291ae8088e2eaef51f0e
camivasz <mvasqu49@eafit.edu.co>
2024-06-25 23:16:10 -0500

    feature/read data

--- main.py
+++ main.py
@@ -36,7 +36,37 @@ def get_last_week(data):
     pass
 
 def main():
-    pass
+    # Create a SparkSession
+    spark = SparkSession.builder.appName("descubri_mas").getOrCreate()
+    # Read the JSON files
+    prints = read_json("prints.json")
+    prints = prints.select(
+        col("day"),
+        col("event_data.position").alias("position"),
+        col("event_data.value_prop").alias("value_prop"),
+        col("user_id"),
+    )
+    prints.show()
+    taps = read_json("taps.json")
+    taps = taps.select(
+        col("day"),
+        col("event_data.position").alias("position"),
+        col("event_data.value_prop").alias("value_prop"),
+        col("user_id"),
+    ).selectExpr(
+        "day as tap_day",
+        "position as tap_position",
+        "value_prop as tap_value_prop",
+        "user_id as tap_user_id",
+    )
+    # read the csv file
+    payments = spark.read.csv("pays.csv", header=True)
+    payments = payments.selectExpr(
+        "pay_date",
+        "total",
+        "user_id as pay_user_id",
+        "value_prop as pay_value_prop",
+    )
 
 if __name__=='__main__':
     main()
